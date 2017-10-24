9e5bf13535731d9c8bf9aaa79ffa9eb1baace220
hiroshiba <hihokaruta@gmail.com>
2017-10-25 07:04:11 +0900

    get secret

--- config/config.py
+++ config/config.py
@@ -4,6 +4,7 @@ from typing import List, NamedTuple
 
 
 class Config(NamedTuple):
+    api_base_url: str
     path_html5_comment_generator: str
     execute_command: List[str]
     highlight: List[str]
@@ -16,6 +17,7 @@ class Config(NamedTuple):
 def generate_from_file(path):
     info = json.load(open(path, encoding='utf-8'))
     return Config(
+        api_base_url=info['api_base_url'],
         path_html5_comment_generator=info['path_html5_comment_generator'],
         execute_command=info['execute_command'],
         highlight=info['highlight'],
